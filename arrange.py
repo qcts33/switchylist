ef68817531b11efe06495199f53f9c37a4e84322
qcts33 <qcthreestones@gmail.com>
2020-02-16 23:22:23 +0800

    add optimize

--- arrange.py
+++ arrange.py
@@ -1,3 +1,5 @@
+import copy
+
 import fire
 
 
@@ -33,5 +35,19 @@ def merge(file_name: str):
     wirte_sorl(head, body + new)
 
 
+def optimize():
+    head, body = load_sorl("arrange.sorl")
+    copyed = copy.copy(body)
+    body = list(b[1:] for b in body)
+    filtered = []
+    for outer, orig in zip(body, copyed):
+        for inner in body:
+            if outer.endswith(inner) and outer != inner:
+                break
+        else:
+            filtered.append(orig)
+    wirte_sorl(head, filtered)
+
+
 if __name__ == "__main__":
-    fire.Fire({"buildfrom": build_from, "merge": merge})
+    fire.Fire({"buildfrom": build_from, "merge": merge, "optimize": optimize})
